1b157b2bcd2401afe203d3b9a36086466e674e26
rajeshrangineni <ranginenirajesh@gmail.com>
2015-10-07 23:19:52 +0530

    my first commit

---
+++ src/corejava/Demo.java
@@ -0,0 +1,40 @@
+package corejava;
+
+public class Demo {
+
+	static String raj="naresh";
+    String raje;
+
+   /* Demo(){
+    	System.out.println("dgffdisfd");
+    }
+    */
+    
+		 void m1(){
+
+		   String aaa="rajesh";
+		   raje="hi";
+		 System.out.println(aaa);
+		 System.out.println(raje);
+		  System.out.println(raj);
+		  
+		 }
+		 /*static void m2(){ 
+			 Demo d2=new Demo();
+			 
+			
+		 }*/
+		
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Demo d1=new Demo();
+       
+       System.out.println(d1.raje);
+       d1.m1();
+      
+      
+     
+	}
+
+}
